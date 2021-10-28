9d044dbffa93ff699b6832227b2136a5360a07e8
Tanvir Sayed <tsayed271@gmail.com>
2021-10-28 13:20:11 +0200

    parse_csv1 for testing

---
+++ code/include/parse_csv1.h
@@ -0,0 +1,9 @@
+#ifndef PARSE_CSV2_H
+#define PARSE_CSV2_H
+#include <csv/csv.h>
+#include <iostream>
+#include <vector>
+#include <string>
+std::vector<Nucleus> parse_Nucleus(const std::string& filename);
+
+#endif // PARSE_CSV_H
