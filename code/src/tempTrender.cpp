1e25b6f16fb28d9e047a0a3366b16d7db762f772
Felipe Abedrapo <felipe.abedrapo.7756@student.lu.se>
2021-10-29 11:09:28 +0200

    Separated big func into respective h and cpp files

--- code/src/tempTrender.cpp
+++ code/src/tempTrender.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 #include <string>
 #include "tempTrender.h"
-#include "parse_csv.h"
 
 tempTrender::tempTrender(const std::string& filePath) {
 	std::cout << "The user supplied " << filePath <<" as the path to the data file.\n";
-	std::cout << "You should probably store this information in a member variable of the class! Good luck with the project! :)\n";
+	//std::cout << "You should probably store this information in a member variable of the class! Good luck with the project! :)\n";
 	std::cout << "filepath is stored in member variable called _filepath \n";
 	_filepath = filePath;
 }
 
-/*
-reading the csv(){
-	...
-	...
-	...
-	return std::string date;
-}
+void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const{
+	// Converting integer month and day to string for comparing with Datum column
+	std::string _month;
+	std::string _day;
+	std::string test_string;
 
-*/
+	// Making sure month and day is in the correct format
+	if (monthToCalculate < 10){_month = "0" + std::to_string(monthToCalculate);}
+	else{_month = std::to_string(monthToCalculate);}
+	if (dayToCalculate < 10){_day = "0" + std::to_string(dayToCalculate);}
+	else{_day = std::to_string(dayToCalculate);}
+
+	// Final String
+	test_string = _month + "-" + _day;
+
+	// Parsing through csv and adding desired information to info_vector
+	std::vector<double> info_vector;
+	std::string filename = _filepath;
+	info_vector = extract_temp_for_month_day (test_string, filename);
+
+	// Printing to make sure things work
+	std::cout << "First element of info_vector "<< info_vector[0] << "\n";
+	std::cout << "Now we start making the hist... \n";
+}
 
 //Make a histogram of the temperature on this day
 
