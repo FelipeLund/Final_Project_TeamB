3a44544f611d2009358eba5732d6fccc0eadf6db
FelipeAbedrapo <91557778+FelipeLund@users.noreply.github.com>
2021-10-30 12:14:54 +0200

    Delete parse_csv1.h

--- code/include/parse_csv1.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSE_CSV2_H
-#define PARSE_CSV2_H
-#include <fast-cpp-csv-parser/csv.h>
-#include <iostream>
-#include <vector>
-#include <string>
-std::vector<string> parse_smhi(const std::string& filename);
-
-#endif // PARSE_CSV_H
