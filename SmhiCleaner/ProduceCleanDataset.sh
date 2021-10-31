71f8724b5de291c6e3d57cfe25663ec55b742211
Tanvir Sayed <tsayed271@gmail.com>
2021-10-31 14:05:36 +0100

    Adjusted to code so it stores the clean datasets in ClnData folder

--- SmhiCleaner/ProduceCleanDataset.sh
+++ SmhiCleaner/ProduceCleanDataset.sh
@@ -6,6 +6,8 @@ CITY=$2
 
 Og=`pwd` #recording original directory where script is run
 #echo $Og
+
+
 cd $DATAPATH
 CITYFILE="$DATAPATH/`grep -i -l -m 2 $CITY *`" # - m 2
 echo $CITYFILE
@@ -14,7 +16,8 @@ if [[ "x$CITYFILE" == "x$DATAPATH/" ]]; then
 	echo "City $CITY not found, exiting"
 	exit 1
 fi
-#//make a temporary directory that is deleted in the end 
+#//make a temporary directory that is deleted end of script
+mkdir $Og/SmhiCleaner/CleanerOutputs
 cd $Og/SmhiCleaner/CleanerOutputs
 
 $Og/SmhiCleaner/smhicleaner.sh $CITYFILE > ./shmicleaner.log 2>&1
@@ -23,8 +26,10 @@ RAWFILENAME=$(basename $CITYFILE)
 
 RAWFILENAME="rawdata_${RAWFILENAME}"
 
-cp $RAWFILENAME $Og/CleanDatasets/Clean$CITY
+cp $RAWFILENAME $Og/code/ClnData/Clean$CITY.csv
 
 rm *
 
 cd $Og
+
+rmdir $Og/SmhiCleaner/CleanerOutputs
