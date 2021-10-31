53d910274000fb15b52fe4343e610b448c002c64
Simon Tropp <si8054tr-s@student.lu.se>
2021-10-31 14:46:07 +0100

    Cleand the output

--- code/src/tempTrender.cpp
+++ code/src/tempTrender.cpp
@@ -4,9 +4,8 @@
 #include "tempTrender.h"
 
 tempTrender::tempTrender(const std::string& filePath) {
-	std::cout << "The user supplied " << filePath <<" as the path to the data file.\n";
-	//std::cout << "You should probably store this information in a member variable of the class! Good luck with the project! :)\n";
-	std::cout << "filepath is stored in member variable called _filepath \n";
+	std::cout << ">>> The user supplied " << filePath <<" as the path to the data file.\n\n";
+	//store in member variable called _filepath
 	_filepath = filePath;
 }
 
@@ -32,7 +31,6 @@ void tempTrender::create_hist(std::vector<double> vector_of_degrees) const{
 
 
 void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const{
-	std::cout << "Starting tempOnDay Function... \n";
 
 	// Converting integer month and day to string for comparing with Datum column
 	std::string _month;
@@ -40,13 +38,23 @@ void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const{
 	std::string test_string;
 
 	// Making sure month and day is in the correct format
-	if (monthToCalculate < 10){_month = "0" + std::to_string(monthToCalculate);}
-	else{_month = std::to_string(monthToCalculate);}
-	if (dayToCalculate < 10){_day = "0" + std::to_string(dayToCalculate);}
-	else{_day = std::to_string(dayToCalculate);}
+	if (monthToCalculate < 10){
+		_month = "0" + std::to_string(monthToCalculate);
+	}
+	else{
+		_month = std::to_string(monthToCalculate);
+	}
+
+	if (dayToCalculate < 10){
+		_day = "0" + std::to_string(dayToCalculate);
+	}
+	else{
+		_day = std::to_string(dayToCalculate);
+	}
 
 	// Final String
 	test_string = _month + "-" + _day;
+	std::cout << ">>> The user supplied the date (mm-dd): " << test_string << "\n\n";
 
 	// Parsing through csv and adding desired information to info_vector
 	std::vector<double> info_vector;
@@ -55,30 +63,28 @@ void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const{
 
 
 	// Creating and printing the histogram
-	std::cout << "Now making a histogram.\n";
+	std::cout << "Making the histogram...\n\n";
 	create_hist(info_vector);
 }
 
 
 void tempTrender::maxTempOverTime()const {
-	std::cout << "Max Temp Over Time starting... \n";
 	std::vector<double> max_temps;
 	std::string filename = _filepath;
 	max_temps = maxTempInYears(filename);
-	std::cout << "First element of max_temps vector: "<< max_temps[0] <<  "\n";
-	std::cout << "Plotting Max Temp Over Time starting... \n";
+	std::cout << "Plotting Max Temp Over Time... \n\n";
 	
 	// Plotting the data
 	std::vector<double> years = getYearsList(filename);
 	TCanvas* c2 = new TCanvas();
 	int n = years.size();
-	max_temps.pop_back();
+	//max_temps.pop_back();
 	TGraph *gr = new TGraph(n, &years[0], &max_temps[0]);
    	gr->SetTitle("Max Temperatures every year");
 	gr->GetXaxis()->SetTitle("Time [y]");
 	gr->GetYaxis()->SetTitle("Max Temperature [#circ C]");
 	gr->SetLineColor(6);
-    //gr->SetLineWidth(2);
+    //gr->SetLineWidth(2); //un-comment if thicker lines are preferred
     gr->SetMarkerColor(4);
     gr->SetMarkerStyle(5);
    	gr->Draw();
@@ -88,26 +94,24 @@ void tempTrender::maxTempOverTime()const {
 }
 
 void tempTrender::dailyTempOverTime()const{
-	std::cout << "Getting average daily temp for every single day... \n";
 	std::vector<double> dailyTempOvertime = getDailyTempOverTime(_filepath);
 	int n = dailyTempOvertime.size();
-	std::cout << "Number of days: " << n << "\n";
+	std::cout << "Plotting average daily temp for " << n << " days...\n\n";
 
 	// Plotting the data
 	double start = 1;
 	std::vector<double> days_passed(n);
 	std::iota(days_passed.begin(), days_passed.end(), start);
 	TCanvas* c3 = new TCanvas();
-	c3->SetCanvasSize(3000, 500); //width,height
+	c3->SetCanvasSize(3000, 500); //width, height
 
 	TGraph *gr = new TGraph(n, &days_passed[0], &dailyTempOvertime[0]);
    	gr->SetTitle("Average Daily Temperatures over time");
-	gr->GetXaxis()->SetTitle("Time [d]");
+	gr->GetXaxis()->SetTitle("Time [days]");
 	gr->GetYaxis()->SetTitle("Average Temperature [#circ C]");
 	gr->SetLineColor(6);
-    //gr->SetLineWidth(2);
     gr->SetMarkerColor(4);
-    //gr->SetMarkerStyle(5); //looks too cluttered 
    	gr->Draw("AP");
 	
+	std::cout << "---------------------- Done! ----------------------\n";
 }
