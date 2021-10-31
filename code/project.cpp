bfce164404007531a70e1c867755c22655f6e6db
Simon Tropp <si8054tr-s@student.lu.se>
2021-10-31 11:34:43 +0100

    Added interface and made program interactive

--- code/project.cpp
+++ code/project.cpp
@@ -6,10 +6,25 @@
 
 
 int main() {
-	const std::string pathToFile = "./ClnData/CleanLund.csv"; //path to data file
+	std::string pathToFile; //path to data file
 
+	//Interface and interactive
+	std::cout << "\n-------------------- Welcome! --------------------\n";
+	std::cout << "This program will: \n";
+	std::cout << "(1) Print a histogram of temperatures on a specific day through time. \n(2) Show a plot of the maximum temperature each year. \n(3) Show a plot of the mean temperature on every day throughout time.\n\n";
+
+	std::cout << "First, enter the path of a clean data file (e.g. ./ClnData/CleanLund.csv): ";
+	std::cin >> pathToFile;
+	
 	tempTrender t(pathToFile); //Instantiate your analysis object
-	t.tempOnDay(12, 15);
+	
+	std::cout << "Now, enter a month and day for which you want to see a histogram of temperatures through time (mm dd): ";
+
+	int mm, dd;
+	std::cin >> mm >> dd;
+	
+
+	t.tempOnDay(mm, dd);
 	t.maxTempOverTime();
 	t.dailyTempOverTime();
 
