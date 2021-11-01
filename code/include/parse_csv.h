f9b24413388ee2d4643ce2a1aeb2c19ab5498415
Simon Tropp <si8054tr-s@student.lu.se>
2021-11-01 12:09:39 +0100

    Added code for plotting the time between peaks in dailyTempOver time (not working as intended)

--- code/include/parse_csv.h
+++ code/include/parse_csv.h
@@ -8,6 +8,8 @@ std::vector<double> extract_temp_for_month_day (const std::string test_string, c
 
 std::vector<double> maxTempInYears(std::string& filename);
 
+std::vector<int> max_temp_positions(const std::string& filename);
+
 std::vector<double> getYearsList(const std::string& filename);
 
 std::vector<std::string> getAllDatums(const std::string& filename);
